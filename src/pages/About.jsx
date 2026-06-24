c3a616fb55997ac185af42f2ecbe522df9888df1
cartonmouse <clearsnowsong@gmail.com>
2026-06-24 14:35:12 +0800

    修改关于页面的邮箱

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ export default function About() {
     <main className="section-shell page-top about-page about-minimal">
       <h1>Amor Fati</h1>
       <p>
-        <a href="mailto:paperboxmouse@163.com">paperboxmouse@163.com</a>
+        <a href="mailto:camerasnowsong@163.com">camerasnowsong@163.com</a>
         <span>通过这个邮箱可以联系我</span>
       </p>
     </main>
